09600bc4921e43ff11a0a273cfc274a4db8be039
Valeroschka <valerij.arhipov@web.de>
2025-11-23 19:16:46 +0100

    Register now working => New SecurityConfig

---
+++ backend/guitarapp/src/main/java/com/project_string/guitarapp/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.project_string.guitarapp.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        // hier erlauben wir beide Varianten
+                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
